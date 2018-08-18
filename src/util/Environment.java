b34628c7553f6310daa688ebece54d30314f59b0
DarthVi <covinc93@gmail.com>
2018-08-18 10:53:54 +0200

    Defined STentry class (symbol table entry), startet Environment class (not yet finished)

---
+++ src/util/Environment.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import ast.STentry;
+
+public class Environment
+{
+
+    private ArrayList<HashMap<String,STentry>> symTable = new ArrayList<HashMap<String,STentry>>();
+}
