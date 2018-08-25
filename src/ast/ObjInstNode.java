1e0d54581fee47c4faa519a4b6f36ecaa39c47ff
DarthVi <covinc93@gmail.com>
2018-08-25 19:06:59 +0200

    added ObjInstNode for object instantiation. However it must be completed

---
+++ src/ast/ObjInstNode.java
@@ -0,0 +1,47 @@
+package ast;
+
+import exception.TypeException;
+import type.ClassType;
+import type.IType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class ObjInstNode implements INode
+{
+    private String objectName;
+    private ClassType classType;
+
+    public ObjInstNode(String objectName, ClassType classType)
+    {
+        this.objectName = objectName;
+        this.classType = classType;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        return this.classType;
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        //TODO: controllare che non vengano ridefiniti i simboli della superclasse
+        //TODO: aggiungere questa classe e il suo nome/simbolo all'environment
+        return null;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString();
+    }
+}
