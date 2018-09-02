837f587ce7b1726d43964d2f078625d92a2fc9dc
DarthVi <covinc93@gmail.com>
2018-09-02 15:39:39 +0200

    Added code (specifically typeCheck(), members and constructors; codeGeneration() and checkSemantics() not yet completed) GreaterNode, LessNode, EqualNode, GreaterEqualNode, LessEqualNode.

--- src/ast/GreaterEqualNode.java
+++ src/ast/GreaterEqualNode.java
@@ -1,4 +1,53 @@
 package ast;
 
-public class GreaterEqualNode {
+import exception.TypeException;
+import org.antlr.v4.runtime.ParserRuleContext;
+import type.BoolType;
+import type.IType;
+import type.IntType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class GreaterEqualNode implements INode {
+
+    private INode left;
+    private INode right;
+    private ParserRuleContext ctx;
+
+    public GreaterEqualNode(INode left, INode right, ParserRuleContext ctx)
+    {
+        this.left = left;
+        this.right = right;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        IType l = left.typeCheck();
+        IType r = right.typeCheck();
+
+        if(!(l.isSubtypeOf(new IntType()) && r.isSubtypeOf(new IntType())))
+        {
+            throw new TypeException("Type error: >= operator allowed only on int", ctx);
+        }
+
+        return new BoolType();
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        //TODO
+        return null;
+    }
 }
--- src/ast/GreaterNode.java
+++ src/ast/GreaterNode.java
@@ -1,4 +1,52 @@
 package ast;
 
-public class GreaterNode {
+import exception.TypeException;
+import org.antlr.v4.runtime.ParserRuleContext;
+import type.BoolType;
+import type.IType;
+import type.IntType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class GreaterNode implements INode{
+    private INode left;
+    private INode right;
+    private ParserRuleContext ctx;
+
+    public GreaterNode(INode left, INode right, ParserRuleContext ctx)
+    {
+        this.left = left;
+        this.right = right;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        IType l = left.typeCheck();
+        IType r = right.typeCheck();
+
+        if(!(l.isSubtypeOf(new IntType()) && r.isSubtypeOf(new IntType())))
+        {
+            throw new TypeException("Type error: > operator allowed only on int", ctx);
+        }
+
+        return new BoolType();
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        //TODO
+        return null;
+    }
 }
