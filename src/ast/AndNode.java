d9764ec0725aba75243364dfd05f12c10c1c09e5
DarthVi <covinc93@gmail.com>
2018-09-02 16:42:11 +0200

    Coded typeCheck() for AndNode and OrNode

--- src/ast/AndNode.java
+++ src/ast/AndNode.java
@@ -1,4 +1,51 @@
 package ast;
+import exception.TypeException;
+import org.antlr.v4.runtime.ParserRuleContext;
+import type.BoolType;
+import type.IType;
+import util.Environment;
+import util.SemanticError;
 
-public class AndNode {
+import java.util.ArrayList;
+
+public class AndNode implements INode
+{
+    private INode left;
+    private INode right;
+    private ParserRuleContext ctx;
+
+    public AndNode(INode left, INode right, ParserRuleContext ctx)
+    {
+        this.left = left;
+        this.right = right;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        IType l = left.typeCheck();
+        IType r = right.typeCheck();
+
+        if(!(l.isSubtypeOf(new BoolType()) && r.isSubtypeOf((new BoolType()))))
+        {
+            throw new TypeException("Type error: AND operator works only between booleans", ctx);
+        }
+
+        return new BoolType();
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        //TODO
+        return null;
+    }
 }
--- src/ast/OrNode.java
+++ src/ast/OrNode.java
@@ -1,4 +1,52 @@
 package ast;
 
-public class OrNode {
+import exception.TypeException;
+import org.antlr.v4.runtime.ParserRuleContext;
+import type.BoolType;
+import type.IType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class OrNode implements INode
+{
+    private INode left;
+    private INode right;
+    private ParserRuleContext ctx;
+
+    public OrNode(INode left, INode right, ParserRuleContext ctx)
+    {
+        this.left = left;
+        this.right = right;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        IType l = left.typeCheck();
+        IType r = right.typeCheck();
+
+        if(!(l.isSubtypeOf(new BoolType()) && r.isSubtypeOf((new BoolType()))))
+        {
+            throw new TypeException("Type error: AND operator works only between booleans", ctx);
+        }
+
+        return new BoolType();
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        //TODO
+        return null;
+    }
 }
