19399832eca09ea5add487cf1a09dc16c3e16623
lorenzomax <lorenzomassimiliani@gmail.com>
2018-09-02 18:38:51 +0200

    added CodeGeneration for Equal/Greater/Less/GE/LE nodes

--- src/ast/GreaterEqualNode.java
+++ src/ast/GreaterEqualNode.java
@@ -1,4 +1,47 @@
 package ast;
 
-public class GreaterEqualNode {
+import exception.TypeException;
+import lib.FOOLlib;
+import type.IType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class GreaterEqualNode implements INode {
+
+    private INode left;
+    private INode right;
+
+    public GreaterEqualNode(INode left, INode right) {
+        this.left = left;
+        this.right = right;
+    }
+
+
+    @Override
+    public IType typeCheck() throws TypeException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        return null;
+    }
+
+    @Override
+    public String codeGeneration() {
+        String l1 = FOOLlib.freshLabel();
+        String l2 = FOOLlib.freshLabel();
+
+        return right.codeGeneration() +
+                left.codeGeneration() +
+                "bleq " + l1 + "\n" +
+                "push 0\n" +
+                "b " + l2 + "\n" +
+                l1 + ":\n" +
+                "push 1\n" +
+                l2 + ":\n";
+    }
+
 }
--- src/ast/LessEqualNode.java
+++ src/ast/LessEqualNode.java
@@ -1,4 +1,45 @@
 package ast;
 
-public class LessEqualNode {
+import exception.TypeException;
+import lib.FOOLlib;
+import type.IType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class LessEqualNode implements INode {
+
+    private  INode right;
+    private  INode left;
+
+    public LessEqualNode(INode left, INode right) {
+        this.left = left;
+        this.right = right;
+
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        return null;
+    }
+
+    @Override
+    public String codeGeneration() {
+        String l1 = FOOLlib.freshLabel();
+        String l2 = FOOLlib.freshLabel();
+        return left.codeGeneration() +
+                right.codeGeneration() +
+                "bleq " + l1 + "\n" +
+                "push 0\n" +
+                "b " + l2 + "\n" +
+                l1 + ":\n" +
+                "push 1\n" +
+                l2 + ":\n";
+    }
 }
