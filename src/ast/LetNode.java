13128e21c9aa1260afa0473b96c7c97a73da8c0c
DarthVi <covinc93@gmail.com>
2018-09-04 10:09:52 +0200

    LetInNode renamed in LetNode (to avoid confusion with the 'in <scope>' environment). LetInNode.typeCheck() coded

---
+++ src/ast/LetNode.java
@@ -0,0 +1,46 @@
+package ast;
+
+import exception.TypeException;
+import org.antlr.v4.runtime.ParserRuleContext;
+import type.IType;
+import type.VoidType;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class LetNode implements INode{
+
+    private ArrayList<INode> declist;
+    private ParserRuleContext ctx;
+
+    public LetNode(ArrayList<INode> declist, ParserRuleContext ctx)
+    {
+        this.declist = declist;
+        this.ctx = ctx;
+    }
+
+    @Override
+    public IType typeCheck() throws TypeException
+    {
+        for(INode declaration : declist)
+        {
+            declaration.typeCheck();
+        }
+
+        //let <declarations> returns void because each declaration returns void as decided during language design
+        return new VoidType();
+    }
+
+    @Override
+    public String codeGeneration()
+    {
+        return null;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env)
+    {
+        return null;
+    }
+}
