fb0501c2cc5b4d2b4656763215868fec8263c6d5
DarthVi <covinc93@gmail.com>
2018-09-04 11:27:08 +0200

    Fixed VardecNode codegen and added skeleton for other nodes

--- src/ast/ProgLetInNode.java
+++ src/ast/ProgLetInNode.java
@@ -1,27 +1,14 @@
 package ast;
 
 import exception.TypeException;
-import org.antlr.v4.runtime.ParserRuleContext;
 import type.IType;
 import util.Environment;
 import util.SemanticError;
 
 import java.util.ArrayList;
 
-public class VarNode implements INode{
-
-    private String id;
-    private IType type;
-    private INode exp;
-    private ParserRuleContext ctx;
-
-    public VarNode(String id, IType type, INode exp, ParserRuleContext ctx)
-    {
-        this.id = id;
-        this.type = type;
-        this.exp = exp;
-        this.ctx = ctx;
-    }
+public class ProgLetInNode implements INode
+{
 
     @Override
     public IType typeCheck() throws TypeException
