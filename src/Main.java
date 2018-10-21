c8cb07da77ed3933cda363d39e6c8be47e21ea0a
lorenzomax <lorenzomassimiliani@gmail.com>
2018-10-21 17:43:28 +0200

    Edited principal main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import visitors.FOOLVisitorImpl;
 import ast.INode;
 import exception.LexerException;
 import exception.ParserException;
@@ -6,76 +5,105 @@ import exception.SemanticException;
 import exception.TypeException;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
-import parser.FOOLLexer;
-import parser.FOOLParser;
-import parser.DebuggingVisitorImpl;
+import parser.*;
 import type.IType;
 import util.Environment;
 import util.SemanticError;
+import visitors.FOOLVisitorImpl;
+import visitors.FunctionVisitor;
+import visitors.SVMVisitor;
+import vm.VirtualMachine;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+
+//import visitors.SVMVisitorImpl;
+
 
 public class Main {
 
     public static void main(String[] args) {
         try {
 
-            String fileName = "src/test/test.fool";
-            CharStream input = CharStreams.fromFileName(fileName);
+
+            String fileName = "code.fool";
+            CharStream input = CharStreams.fromFileName("src/" +fileName);
+
 
             //LEXER
-            System.out.println("BEGIN LEXICAL ANALYSIS...");
             FOOLLexer lexer = new FOOLLexer(input);
             if (lexer.errors.size() > 0)
                 throw new LexerException(lexer.errors);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
-            for (int i = 0; i < tokens.getNumberOfOnChannelTokens(); i++) {
-                Token token = tokens.get(i);
-                System.out.println(token.getTokenIndex() + " " + token.getText());
-            }
-            System.out.println("...END LEXICAL ANALYSIS\n");
 
-            //PARSER
-            System.out.println("BEGIN SYNTAX ANALYSIS...");
+
+            //PARSING - Syntax analysis
             FOOLParser parser = new FOOLParser(tokens);
             FOOLParser.ProgContext progContext = parser.prog();
             if (parser.getNumberOfSyntaxErrors() > 0)
                 throw new ParserException("Errori rilevati: " + parser.getNumberOfSyntaxErrors() + "\n");
-            System.out.println("...END SYNTAX ANALYSIS\n");
 
-            //SEMANTIC
-            System.out.println("BEGIN SEMANTIC ANALYSIS...\n");
-            DebuggingVisitorImpl debuggingVisitor = new DebuggingVisitorImpl();
-            INode debuggingAst = (INode) debuggingVisitor.visit(progContext);
-            FOOLVisitorImpl visitor = new FOOLVisitorImpl();
-            INode ast = visitor.visit(progContext);
-            Environment env = new Environment();
+
+            FunctionVisitor funVisitor = new FunctionVisitor();
+
+            funVisitor.visit(progContext);
+
+            //we do a little bit of semantic analysis even here, considering however
+            //only function definitions
+            if(funVisitor.getErrorsSize() > 0)
+                throw new SemanticException(funVisitor.getErrors());
+
+            Environment env = funVisitor.getEnvironment();
+
+            FOOLVisitor<INode> visitor = new FOOLVisitorImpl();
+
+            INode ast = (INode) visitor.visit(progContext);
+
+            System.out.println("BEGIN SEMANTIC ANALYSIS...");
             ArrayList<SemanticError> errors = ast.checkSemantics(env);
-            System.out.println(ast.toString());
-            if (errors.size() > 0)
-                throw new SemanticException(errors);
-            System.out.println("\n...END SEMANTIC ANALYSIS");
+            if (errors.size() > 0) throw new SemanticException(errors);
+            System.out.println("...END SEMANTIC ANALYSIS\n");
 
             //TYPE CHECKING
             System.out.println("BEGIN TYPE CHECKING...");
             IType type = ast.typeCheck();
             System.out.println("Type checking: " + type.toString());
-            System.out.println("... END TYPE CHECKING...\n");
+            System.out.println("...END TYPE CHECKING\n");
 
             //CODE GENERATION
             System.out.println("BEGIN CODE GENERATION...");
             String code = ast.codeGeneration();
-            /*File svmFile = new File("code.svm");
-            BufferedWriter svmWriter = new BufferedWriter(new FileWriter(svmFile.getAbsoluteFile()));
-            svmWriter.write(code);
-            svmWriter.close();*/
-            System.out.println("END CODE GENERATION...\n");
+            System.out.println(code);
+            System.out.println("...END CODE GENERATION\n");
 
-        } catch (IOException | LexerException | ParserException | SemanticException | TypeException e) {
+            BufferedWriter out = new BufferedWriter(new FileWriter("src/" + fileName +".asm"));
+            out.write(code);
+            out.close();
+
+            //CODE EXECUTION
+            CodePointCharStream isASM = (CodePointCharStream) CharStreams.fromFileName("src/" + fileName +".asm");
+            SVMLexer lexerASM = new SVMLexer(isASM);
+
+            CommonTokenStream tokensASM = new CommonTokenStream(lexerASM);
+            SVMParser parserASM = new SVMParser(tokensASM);
+            SVMParser.CodeContext codeContext = parserASM.code();
+            System.out.println(parserASM.code());
+
+            SVMVisitor svmVisitor = new SVMVisitor();
+            svmVisitor.visit(codeContext);
+
+            VirtualMachine vm = new VirtualMachine(svmVisitor.getCode());
+            System.out.println();
+             vm.cpu();
+
+
+        } catch (IOException | LexerException | ParserException | SemanticException | TypeException  e ) {
             System.out.println(e.getMessage());
         }
+
     }
 }
---
+++ src/code.fool.asm
@@ -0,0 +1,15 @@
+push 0
+push 2
+push 3
+push -2
+lfp
+add
+lw
+push -3
+lfp
+add
+lw
+push 1
+add
+add
+halt
