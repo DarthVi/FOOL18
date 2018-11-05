6dd6c9c31554efa96de68a26bd81e69f557013b1
DarthVi <covinc93@gmail.com>
2018-11-05 15:29:08 +0100

    IfNode's typecheck more compact now

--- src/ast/IfNode.java
+++ src/ast/IfNode.java
@@ -36,49 +36,54 @@ public class IfNode implements INode {
         IType thType = th.typeCheck();
         IType elType = el.typeCheck();
 
-        //if this holds, we are using if(condition) then {statements} else {statements}
-        if(thType.isSubtypeOf(new VoidType()) && elType.isSubtypeOf(new VoidType()))
-            return new VoidType();
+//        //if this holds, we are using if(condition) then {statements} else {statements}
+//        if(thType.isSubtypeOf(new VoidType()) && elType.isSubtypeOf(new VoidType()))
+//            return new VoidType();
+//
+//        //if the previous case does not hold, then T1<:T' and T2<:T' and the return type should be T'
+//        //If T1<:T2 then we can return T2 and use it in variable of type T2 or type T' with T2<:T'
+//        //vice versa we return T1.
+//        if(thType.isSubtypeOf(elType))
+//            return elType;
+//
+//        if(elType.isSubtypeOf(thType))
+//            return thType;
 
-        //if the previous case does not hold, then T1<:T' and T2<:T' and the return type should be T'
-        //If T1<:T2 then we can return T2 and use it in variable of type T2 or type T' with T2<:T'
-        //vice versa we return T1.
-        if(thType.isSubtypeOf(elType))
-            return elType;
 
-        if(elType.isSubtypeOf(thType))
+        if(thType.toString().equals(elType.toString()))
             return thType;
-
-        //if none of the previous branch works, we can try to get the closest supertype
-        //to return
-        if(elType instanceof ClassType && thType instanceof ClassType)
         {
-            int elLevel = 0;
-            int thenLevel = 0;
-
-            ClassType currentElP = ((ClassType) elType);
-            ClassType currentThP = ((ClassType) thType);
-
-            while( !(currentElP.getParent() == null & currentThP.getParent() == null) )
+            //if none of the previous branch works, we can try to get the closest supertype
+            //to return
+            if (elType instanceof ClassType && thType instanceof ClassType)
             {
-                if(currentElP.isSubtypeOf(currentThP))
+                int elLevel = 0;
+                int thenLevel = 0;
+
+                ClassType currentElP = ((ClassType) elType);
+                ClassType currentThP = ((ClassType) thType);
+
+                while (!(currentElP.getParent() == null & currentThP.getParent() == null))
+                {
+                    if (currentElP.isSubtypeOf(currentThP))
+                        return currentThP;
+
+                    if (currentThP.isSubtypeOf(currentElP))
+                        return currentElP;
+
+                    if (currentElP.getParent() != null)
+                        currentElP = currentElP.getParent();
+
+                    if (currentThP.getParent() != null)
+                        currentThP = currentThP.getParent();
+                }
+
+                if (currentElP.isSubtypeOf(currentThP))
                     return currentThP;
 
-                if(currentThP.isSubtypeOf(currentElP))
+                if (currentThP.isSubtypeOf(currentElP))
                     return currentElP;
-
-                if(currentElP.getParent() != null)
-                    currentElP = currentElP.getParent();
-
-                if(currentThP.getParent() != null)
-                    currentThP = currentThP.getParent();
             }
-
-            if(currentElP.isSubtypeOf(currentThP))
-                return currentThP;
-
-            if(currentThP.isSubtypeOf(currentElP))
-                return currentElP;
         }
 
         throw new TypeException("Type error in then and else branches", ctx);
--- src/code.fool.asm
+++ src/code.fool.asm
@@ -1,15 +1,77 @@
 push 0
-push 2
-push 3
+push 0
+push classB
+new
+push 0
+push classC
+new
+push 1
+push 1
+beq label0
 push -2
 lfp
 add
 lw
+b label1
+label0:
 push -3
 lfp
 add
 lw
-push 1
+label1:
+lfp
+push -4
+lfp
 add
+lw
+copy
+lw
+push 0
 add
+lc
+js
+print
 halt
+
+function0:
+cfp
+lra
+push 1
+srv
+sra
+pop
+sfp
+lrv
+lra
+js
+
+function1:
+cfp
+lra
+push 2
+srv
+sra
+pop
+sfp
+lrv
+lra
+js
+
+function2:
+cfp
+lra
+push 3
+srv
+sra
+pop
+sfp
+lrv
+lra
+js
+classA:
+function0
+classB:
+function1
+classC:
+function2
+classnull:
